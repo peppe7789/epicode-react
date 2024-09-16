267d63d57c81867c6a3357ea3caa8f9d8536ad0a
peppe7789 <165323825+peppe7789@users.noreply.github.com>
2024-09-16 22:31:31 +0200

    modificata lista di card con inserimento il AllBooks del componente Singlebook

--- src/components/AllTheBooks/AllTheBooks.jsx
+++ src/components/AllTheBooks/AllTheBooks.jsx
@@ -1,6 +1,7 @@
 import { AccordionItem, Col, Container, Row } from "react-bootstrap"
 import fantasy from "../../costants/books/fantasy.json"
 import BookCard from "./BookCard/BookCard"
+import SingleBook from "./SingleBook/SingleBook"
 
 
 const AllTheBooks = () => {
@@ -8,10 +9,8 @@ const AllTheBooks = () => {
         <Container>
             <Row className="gy-4 d-flex gx">
             {fantasy.map((book) => (
-                    <BookCard
+                    <SingleBook
                         title={book.title}
-                        price={book.price}
-                        category={book.category}
                         img={book.img}
                     />
                 ))}
--- src/components/AllTheBooks/SingleBook/SingleBook.jsx
+++ src/components/AllTheBooks/SingleBook/SingleBook.jsx
@@ -1,3 +1,4 @@
+import { Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
@@ -5,12 +6,17 @@ import Card from 'react-bootstrap/Card';
 
 const SingleBook = ({ img, title }) => {
     return (
-        <Card >
-            <Card.Img variant="top" src={img} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-            </Card.Body>
-        </Card>
+        <Col
+            sm
+            md={3}
+        >
+            <Card >
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                </Card.Body>
+            </Card>
+        </Col>
     )
 }
 
